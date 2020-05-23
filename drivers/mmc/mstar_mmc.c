45ea814da10a911a4fa8e72d4ff253ce8b9feb93
Daniel Palmer <daniel@0x0f.com>
2020-05-23 15:12:12 +0900

    mstar mmc progress

--- drivers/mmc/mstar_mmc.c
+++ drivers/mmc/mstar_mmc.c
@@ -10,28 +10,424 @@
 #include <mmc.h>
 #include <reset.h>
 #include <asm/io.h>
+#include <regmap.h>
+
+struct reg_field {
+	unsigned offset;
+	unsigned lsb;
+	unsigned msb;
+};
+
+
+#define REG_FIELD(_offset, _lsb, _msb) \
+{ \
+	.offset = _offset,\
+	.lsb = _lsb,\
+	.msb = _msb,\
+}
+
+
+struct regmap_field {
+	struct regmap	*regmap;
+	uint offset;
+	uint shift;
+	uint32_t mask;
+};
+
+static int regmap_field_write(struct regmap_field *field, uint value)
+{
+	printf("field write, off %x, mask %x, shift %d, value %x, masked value %x\n", (unsigned) field->offset,
+			(unsigned)field->mask, (unsigned) field->shift, (unsigned) value, (unsigned)(value & field->mask));
+	return regmap_update_bits(field->regmap, field->offset, field->mask, value << field->shift);
+}
+
+static int regmap_field_read(struct regmap_field *field, uint* value)
+{
+	uint temp;
+	int ret = 0;
+
+	printf("field read, off %x, mask %x, shift %d, value %x\n", (unsigned) field->offset,
+			(unsigned) field->mask, (unsigned) field->shift, (unsigned) value);
+	ret = regmap_read(field->regmap, field->offset, &temp);
+	temp &= field->mask;
+	*value = temp >> field->shift;
+
+	return ret;
+}
+
+static struct regmap_field* regmap_field_alloc(struct regmap *regmap, struct reg_field field)
+{
+	struct regmap_field* f;
+	f = malloc(sizeof(*f));
+	f->regmap = regmap;
+	f->offset = field.offset;
+	f->shift = field.lsb;
+	f->mask = GENMASK(field.msb, field.lsb);
+	return f;
+}
+
+#define regmap_field_read_poll_timeout(field, val, cond, sleep_us, \
+				      timeout_ms) \
+({ \
+	unsigned long __start = get_timer(0); \
+	int __ret; \
+	for (;;) { \
+		__ret = regmap_field_read((field), &(val)); \
+		if (__ret) \
+			break; \
+		if (cond) \
+			break; \
+		if ((timeout_ms) && get_timer(__start) > (timeout_ms)) { \
+			__ret = regmap_field_read((field), &(val)); \
+			break; \
+		} \
+		if ((sleep_us)) \
+			udelay((sleep_us)); \
+	} \
+	__ret ?: ((cond) ? 0 : -ETIMEDOUT); \
+})
+
+#include "mstar-fcie.h"
 
 struct mstar_mmc_platdata {
-
+	struct mmc_config cfg;
+	struct mmc mmc;
 };
 
 struct mstar_mmc_priv {
+	struct regmap	*regmap;
 
+	// io control
+	struct regmap_field *clk_en;
+	struct regmap_field *bus_width;
+
+	// transfer control
+	struct regmap_field *blk_sz;
+	struct regmap_field *blk_cnt;
+	struct regmap_field *rspr2_en;
+	struct regmap_field *rsp_en;
+	struct regmap_field *cmd_en;
+	struct regmap_field *dtrf_en;
+	struct regmap_field *jobdir;
+	struct regmap_field *adma_en;
+	struct regmap_field *busydet_en;
+	struct regmap_field *errdet_en;
+	struct regmap_field *cmd_sz;
+	struct regmap_field *rsp_sz;
+	struct regmap_field *job_start;
+
+	struct regmap_field *dmaaddr_l;
+	struct regmap_field *dmaaddr_h;
+	struct regmap_field *dmalen_l;
+	struct regmap_field *dmalen_h;
+
+	// status
+	struct regmap_field *status;
+	struct regmap_field *card_busy;
+
+	// reset
+	struct regmap_field *nrst;
+	struct regmap_field *rst_status;
+
+	//
+	bool cmd_done;
+	bool data_done;
+	bool busy_done;
+	bool error;
 };
 
 static int mstar_mmc_set_ios(struct udevice *dev)
 {
+	printf("%s:%d\n", __FUNCTION__, __LINE__);
 	return 1;
+}
+
+static void mstar_mmc_send_cmd_writecmd(struct mstar_mmc_priv* priv, u8 cmd, u32 arg){
+	int i;
+	u8 fifo[6] = {0};
+	u8 *bytes = fifo;
+	u16 *words = (u16*) fifo;
+
+	*bytes++ = cmd | 0x40 ;
+
+	arg = cpu_to_be32(arg);
+	memcpy(bytes, &arg, sizeof(arg));
+
+	for(i = 0; i < ARRAY_SIZE(fifo) / 2; i++){
+		regmap_write(priv->regmap, REG_FIFO + (i * 4), words[i]);
+	}
+}
+
+static int mstar_mmc_setupcmd(struct mstar_mmc_priv* priv, struct mmc_cmd *cmd){
+	int rspsz = 0;
+
+	// clear any existing flags
+	regmap_write(priv->regmap, REG_SD_CTL, 0);
+
+	// load the command into the fifo
+	mstar_mmc_send_cmd_writecmd(priv, cmd->cmdidx, cmd->cmdarg);
+
+	// configure the response length
+	regmap_field_write(priv->rsp_en, 0);
+	regmap_field_write(priv->rspr2_en, 0);
+	if(cmd->resp_type & MMC_RSP_PRESENT){
+		regmap_field_write(priv->rsp_en, 1);
+		if(cmd->resp_type & MMC_RSP_136){
+			regmap_field_write(priv->rspr2_en, 1);
+			rspsz = 16;
+		}
+		else
+			rspsz = 5;
+	}
+
+	regmap_field_write(priv->busydet_en, cmd->resp_type & MMC_RSP_BUSY ? 1 : 0);
+	regmap_field_write(priv->errdet_en, cmd->resp_type & MMC_RSP_CRC ? 1 : 0);
+	regmap_field_write(priv->cmd_en, 1);
+	regmap_field_write(priv->cmd_sz, 0x5); // this is always the case right?
+	regmap_field_write(priv->rsp_sz, rspsz);
+
+	return rspsz;
+}
+
+static void msc313_fcie_parse_int_flags(struct mstar_mmc_priv *priv, unsigned int flags){
+	printf("int flags %x\n", flags);
+	if(flags & INT_CMD_END){
+		priv->cmd_done = true;
+	}
+	if(flags & INT_DATA_END){
+		priv->data_done = true;
+	}
+	if(flags & INT_BUSY_END){
+		priv->busy_done = true;
+	}
+	if(flags & INT_ERR){
+		priv->error = true;
+	}
+}
+
+static bool mstar_fcie_parse_and_check_flags(struct mstar_mmc_priv *priv, unsigned int flags,
+						bool cmd, bool data, bool busy){
+	bool ret = true;
+	msc313_fcie_parse_int_flags(priv, flags);
+	if(cmd)
+		ret &= priv->cmd_done;
+	if(data)
+		ret &= priv->data_done;
+	if(busy)
+		ret &= priv->busy_done;
+	return ret;
+}
+
+static int mstar_mmc_start_transfer_and_wait(struct mstar_mmc_priv *priv,
+		bool cmd, bool data, bool busy, unsigned int* status){
+	unsigned int intflags, poll_timeout;
+
+	unsigned timeout = 100;
+	if(data)
+		timeout += 1000;
+
+	priv->cmd_done = false;
+	priv->data_done = false;
+	priv->busy_done = false;
+	priv->error = false;
+
+	// clear the flags and start the transfer
+	regmap_field_write(priv->status, ~0);
+	regmap_field_write(priv->job_start, 0);
+	regmap_field_write(priv->job_start, 1);
+
+	/*
+	 * we have to wait some time before polling the flags otherwise
+	 * the controller starts corrupting memory, probably because the
+	 * flags we have are old
+	 */
+	mdelay(100);
+	poll_timeout = regmap_read_poll_timeout(priv->regmap, REG_INT,
+			intflags, mstar_fcie_parse_and_check_flags(priv,intflags,cmd,data,busy), 100, timeout);
+	regmap_write(priv->regmap, REG_INT, ~0);
+	if(poll_timeout){
+		printk("timeout while polling\n");
+		return 1;
+	}
+
+	regmap_field_read(priv->status, status);
+
+	return 0;
+}
+
+static int mstar_mmc_readrsp(struct mstar_mmc_priv *priv, u8 cmd, u32* rsp, int len, bool hasopcode){
+	int i, j;
+	unsigned int value;
+	u8 *buf = (u8*) rsp;
+
+	int words = (len % 2 ? len + 1 : len) / 2;
+
+	for(i = 0; i < words; i++){
+		regmap_read(priv->regmap, REG_FIFO + (i * 4), &value);
+		printk("fifo <- %04x\n", value);
+		for(j = 0; j < 2 && ((i * 2) + j) < len; j++){
+			if(i == 0 && j == 0){
+				// if the first byte is the opcode
+				// check that it matches the expected opcode
+				// as the fifo content could be stale.
+				// This was added because sometimes the
+				// error interrupt was not firing.
+				if(hasopcode && (value & 0xff) != cmd)
+					return EILSEQ;
+
+				// always strip the first byte.
+				continue;
+			}
+			*buf++ = (value >> (8 * j));
+		}
+	}
+
+	rsp[0] = be32_to_cpu(rsp[0]);
+	rsp[1] = be32_to_cpu(rsp[1]);
+	rsp[2] = be32_to_cpu(rsp[2]);
+	rsp[3] = be32_to_cpu(rsp[3]);
+	return 0;
+}
+
+static int mstar_mmc_capturecmdresult(struct mstar_mmc_priv *priv,
+		struct mmc_cmd *cmd, unsigned int status, int rspsz){
+	int err;
+
+
+	if(status & SD_STS_NORSP){
+		err = ETIMEDOUT;
+		return 1;
+	}
+	else if(status & SD_STS_CMDRSPCRCERR){
+		// The vendor driver suggests that the CRC flag
+		// is broken for R3 and R4 responses. But I think
+		// this is for anything without a CRC.
+		if(cmd->cmdarg & MMC_RSP_CRC){
+			//cmd->error = EILSEQ;
+			return 1;
+		}
+	}
+
+	if(rspsz > 0){
+		err = mstar_mmc_readrsp(priv, cmd->cmdidx,
+				cmd->response, rspsz, cmd->cmdarg & MMC_RSP_OPCODE);
+		if(err)
+			return 1;
+	}
+
+	return 0;
+}
+
+static int mstar_mmc_send_cmd_prepcmd_and_tx(struct mstar_mmc_priv *priv, struct mmc_cmd *cmd){
+	int rspsz = mstar_mmc_setupcmd(priv, cmd);
+	unsigned int status;
+	if(mstar_mmc_start_transfer_and_wait(priv, true, false, cmd->cmdarg & MMC_RSP_BUSY, &status)){
+		//cmd->error = ETIMEDOUT;
+	}
+
+	if(mstar_mmc_capturecmdresult(priv, cmd, status, rspsz))
+		return 1;
+
+	return 0;
 }
 
 static int mstar_mmc_send_cmd(struct udevice *dev, struct mmc_cmd *cmd,
 			      struct mmc_data *data)
 {
-	return 1;
+	struct mstar_mmc_priv *priv = dev_get_priv(dev);
+	int rspsz;
+	bool dataread, chkcmddone;
+	uint32_t dmaaddr, dmalen;
+	int ret = 1;
+	unsigned int status, cardbusy;
+
+	printf("%s:%d\n", __FUNCTION__, __LINE__);
+
+	if(data == NULL || (data->flags & MMC_DATA_WRITE)){
+		printf("no data or is write, running command\n");
+		ret = mstar_mmc_send_cmd_prepcmd_and_tx(priv, cmd);
+	}
+
+	if(data){
+		printf("data flags %x\n", data->flags);
+		dataread = data->flags & MMC_DATA_READ;
+		if(dataread){
+			// we're doing a read so setup the command
+			rspsz = mstar_mmc_setupcmd(priv, cmd);
+			regmap_field_write(priv->jobdir, 0);
+			printk("data <- %u x %u\n", data->blocksize, data->blocks);
+		}
+		else {
+			mdelay(100); // this should flush the caches
+			regmap_write(priv->regmap, REG_SD_CTL, 0);
+			// turning on errdet for a write stops the
+			// trigger happening.
+			regmap_field_write(priv->errdet_en, 0);
+			regmap_field_write(priv->jobdir, 1);
+			printk("data -> %u x %u\n", data->blocksize, data->blocks);
+		}
+
+		// setup for data tx/rx
+		regmap_field_write(priv->dtrf_en, 1);
+		regmap_field_write(priv->blk_sz, data->blocksize);
+
+		chkcmddone = dataread;
+		dmaaddr = ((uint32_t) data->dest) - 0x20000000;
+		dmalen = data->blocks * data->blocksize;
+		regmap_field_write(priv->dmaaddr_h, dmaaddr >> 16);
+		regmap_field_write(priv->dmaaddr_l, dmaaddr);
+		regmap_field_write(priv->dmalen_h, dmalen >> 16);
+		regmap_field_write(priv->dmalen_l, dmalen);
+		regmap_field_write(priv->blk_cnt, data->blocks);
+		if(mstar_mmc_start_transfer_and_wait(priv, chkcmddone, true, false, &status)){
+			//mrq->data->error = ETIMEDOUT;
+			ret = 1;
+			goto tfr_err;
+		}
+
+		// the first block will have also triggered sending the cmd
+		// if this was a read so capture the rsp etc for that here
+		// and clear the cmd flags for the next block
+		if(chkcmddone){
+			if(mstar_mmc_capturecmdresult(priv, cmd, status, rspsz))
+				goto tfr_err;
+			regmap_field_write(priv->cmd_en, 0);
+			regmap_field_write(priv->rsp_en, 0);
+			regmap_field_write(priv->rspr2_en, 0);
+			regmap_field_write(priv->busydet_en, 0);
+		}
+
+		// check for errors
+		if(status & SD_STS_DATRDCERR){
+			dev_err(dev, "data read CRC error\n");
+			//mrq->data->error = EILSEQ;
+			ret = 1;
+			goto tfr_err;
+		}
+		if(status & SD_STS_DATWRCERR){
+			dev_err(dev, "data write CRC error\n");
+			//mrq->data->error = EILSEQ;
+			ret = 1;
+			goto tfr_err;
+		}
+
+		// check if the card was still busy
+		// and poll the busy bit until it's not
+		// not totally sure if this is actually
+		// correct.
+		if(status & SD_STS_CARDBUSY){
+			regmap_field_read_poll_timeout(priv->card_busy, cardbusy,
+					!cardbusy, 0, 1000);
+		}
+	}
+
+tfr_err:
+	return ret;
 }
 
 static int mstar_mmc_getcd(struct udevice *dev)
 {
+	printf("%s:%d\n", __FUNCTION__, __LINE__);
 	return 1;
 }
 
@@ -43,12 +439,85 @@ static const struct dm_mmc_ops mstar_mmc_ops = {
 
 static int mstar_mmc_probe(struct udevice *dev)
 {
-	return 1;
+	struct mmc_uclass_priv *upriv = dev_get_uclass_priv(dev);
+	struct mstar_mmc_priv *priv = dev_get_priv(dev);
+	struct mstar_mmc_platdata *plat = dev_get_platdata(dev);
+
+	printf("%s:%d\n", __FUNCTION__, __LINE__);
+
+	plat->mmc.priv = priv;
+	upriv->mmc = &plat->mmc;
+
+	priv->clk_en = regmap_field_alloc(priv->regmap, sd_mode_clken_field);
+	priv->bus_width = regmap_field_alloc(priv->regmap, sd_mode_buswidth_field);
+
+	priv->blk_cnt = regmap_field_alloc(priv->regmap, blockcount_field);
+	priv->blk_sz = regmap_field_alloc(priv->regmap, blocksize_field);
+
+	priv->rspr2_en = regmap_field_alloc(priv->regmap, st_ctl_rspr2en_field);
+	priv->rsp_en = regmap_field_alloc(priv->regmap, st_ctl_rspen_field);
+	priv->adma_en = regmap_field_alloc(priv->regmap, st_ctl_admaen_field);
+	priv->dtrf_en = regmap_field_alloc(priv->regmap, st_ctl_dtrfen_field);
+	priv->jobdir = regmap_field_alloc(priv->regmap, st_ctl_jobdir_field);
+	priv->cmd_en = regmap_field_alloc(priv->regmap, st_ctl_cmden_field);
+	priv->busydet_en = regmap_field_alloc(priv->regmap, st_ctl_busydeten_field);
+	priv->errdet_en = regmap_field_alloc(priv->regmap, st_ctl_errdeten_field);
+
+	priv->cmd_sz = regmap_field_alloc(priv->regmap, cmd_rsp_size_cmdsz_field);
+	priv->rsp_sz = regmap_field_alloc(priv->regmap, cmd_rsp_size_rspsz_field);
+	priv->job_start = regmap_field_alloc(priv->regmap, st_ctl_jobstart_field);
+
+	priv->status = regmap_field_alloc(priv->regmap, sd_sts_status_field);
+	priv->card_busy = regmap_field_alloc(priv->regmap, sd_sts_cardbusy_field);
+
+	priv->nrst = regmap_field_alloc(priv->regmap, rst_nrst_field);
+	priv->status = regmap_field_alloc(priv->regmap, rst_status_field);
+
+	priv->dmaaddr_l = regmap_field_alloc(priv->regmap, dma_addr_l_field);
+	priv->dmaaddr_h = regmap_field_alloc(priv->regmap, dma_addr_h_field);
+	priv->dmalen_l = regmap_field_alloc(priv->regmap, dma_len_l_field);
+	priv->dmalen_h = regmap_field_alloc(priv->regmap, dma_len_h_field);
+
+	return 0;
+}
+
+static int mstar_mmc_ofdata_to_platdata(struct udevice *dev)
+{
+	struct mstar_mmc_priv *priv = dev_get_priv(dev);
+	struct mstar_mmc_platdata *plat = dev_get_platdata(dev);
+	struct mmc_config *cfg;
+	int ret;
+
+	printf("%s:%d\n", __FUNCTION__, __LINE__);
+
+	ret = regmap_init_mem(dev_ofnode(dev), &priv->regmap);
+	if(ret)
+		dev_err(dev, "failed to create reg map\n");
+
+	cfg = &plat->cfg;
+	cfg->name = "MSC";
+	cfg->host_caps = MMC_MODE_HS_52MHz | MMC_MODE_HS;
+
+	ret = mmc_of_parse(dev, cfg);
+	if (ret < 0) {
+		dev_err(dev, "failed to parse host caps\n");
+		return ret;
+	}
+
+	cfg->f_min = 400000;
+	cfg->f_max = 52000000;
+
+	cfg->voltages = MMC_VDD_32_33 | MMC_VDD_33_34;
+	cfg->b_max = CONFIG_SYS_MMC_MAX_BLK_COUNT;
+
+	return 0;
 }
 
 static int mstar_mmc_bind(struct udevice *dev)
 {
-	return 1;
+	struct mstar_mmc_platdata *plat = dev_get_platdata(dev);
+	printf("%s:%d\n", __FUNCTION__, __LINE__);
+	return mmc_bind(dev, &plat->mmc, &plat->cfg);
 }
 
 static const struct udevice_id mstar_mmc_ids[] = {
@@ -59,9 +528,10 @@ static const struct udevice_id mstar_mmc_ids[] = {
 };
 
 U_BOOT_DRIVER(mstar_mmc_drv) = {
-	.name		= "mstar_mmc",
 	.id		= UCLASS_MMC,
+	.name		= "mstar_mmc",
 	.of_match	= mstar_mmc_ids,
+	.ofdata_to_platdata = mstar_mmc_ofdata_to_platdata,
 	.bind		= mstar_mmc_bind,
 	.probe		= mstar_mmc_probe,
 	.ops		= &mstar_mmc_ops,
