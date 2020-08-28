44e662f85ef901a94351aafc5a0804a8282b723d
Daniel Palmer <daniel@0x0f.com>
2020-08-28 23:19:44 +0900

    pinctrl

--- drivers/pinctrl/mstar/pinctrl-mstar.c
+++ drivers/pinctrl/mstar/pinctrl-mstar.c
@@ -7,52 +7,88 @@ DECLARE_GLOBAL_DATA_PTR;
 struct mstar_pinctrl_priv {
 };
 
-static int mstar_pinctrl_get_pins_count(struct udevice *dev)
+struct mstar_pinctrl_group {
+	const char *name;
+};
+
+struct mstar_pinctrl_function {
+	const char *name;
+};
+
+struct mstar_pinctrl_data {
+	const struct mstar_pinctrl_group *groups;
+	unsigned numgroups;
+	const struct mstar_pinctrl_function *functions;
+	unsigned numfunctions;
+};
+
+static const struct mstar_pinctrl_group msc313_groups[] = {
+	{ .name = "eth" },
+};
+
+static const struct mstar_pinctrl_function msc313_functions[] = {
+	{ .name = "eth" },
+};
+
+static const struct mstar_pinctrl_data msc313_data = {
+	.groups = msc313_groups,
+	.numgroups = ARRAY_SIZE(msc313_groups),
+	.functions = msc313_functions,
+	.numfunctions = ARRAY_SIZE(msc313_functions),
+};
+
+static int mstar_pinctrl_get_groups_count(struct udevice *dev)
 {
+	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
+
+	return data->numgroups;
+}
+
+static const char* mstar_pinctrl_get_group_name(struct udevice *dev, unsigned selector)
+{
+	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
+
+	return data->groups[selector].name;
+}
+
+static int  mstar_pinctrl_get_functions_count(struct udevice *dev)
+{
+	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
+
+	return data->numfunctions;
+}
+
+static const char* mstar_pinctrl_get_function_name(struct udevice *dev, unsigned selector)
+{
+	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
+
+	return data->functions[selector].name;
+}
+
+static int mstar_pinctrl_pinmux_group_set(struct udevice *dev, unsigned group_selector, unsigned func_selector)
+{
+	printf("mstar pinctrl g %d, f %d\n", group_selector, func_selector);
+
 	return 0;
 }
 
-static const char *mstar_pinctrl_get_pin_name(struct udevice *dev,
-					      unsigned int selector)
-{
-	return NULL;
-}
-
-static int mstar_pinctrl_get_pin_muxing(struct udevice *dev,
-					unsigned int selector,
-					char *buf,
-					int size)
-{
-	return 0;
-}
+static struct pinctrl_ops mstar_pinctrl_ops = {
+	.get_groups_count = mstar_pinctrl_get_groups_count,
+	.get_group_name = mstar_pinctrl_get_group_name,
+	.get_functions_count = mstar_pinctrl_get_functions_count,
+	.get_function_name = mstar_pinctrl_get_function_name,
+	.pinmux_group_set = mstar_pinctrl_pinmux_group_set,
+	.set_state = pinctrl_generic_set_state,
+};
 
 int mstar_pinctrl_probe(struct udevice *dev)
 {
 	return 0;
 }
 
-static int mstar_pinctrl_set_state(struct udevice *dev, struct udevice *config)
-{
-	return 0;
-}
-
-static int mstar_pinctrl_set_state_simple(struct udevice *dev,
-					  struct udevice *periph)
-{
-	return 0;
-}
-
-static struct pinctrl_ops mstar_pinctrl_ops = {
-	.set_state		= mstar_pinctrl_set_state,
-	.set_state_simple	= mstar_pinctrl_set_state_simple,
-	.get_pin_name		= mstar_pinctrl_get_pin_name,
-	.get_pins_count		= mstar_pinctrl_get_pins_count,
-	.get_pin_muxing		= mstar_pinctrl_get_pin_muxing,
-};
-
 static const struct udevice_id mstar_pinctrl_ids[] = {
-	{ .compatible = "mstar,msc313-pinctrl" },
-	{ .compatible = "mstar,msc313e-pinctrl" },
+	{ .compatible = "mstar,msc313-pinctrl", .data = &msc313_data },
+	{ .compatible = "mstar,msc313e-pinctrl", .data = &msc313_data },
 	{ }
 };
 
@@ -61,7 +97,6 @@ U_BOOT_DRIVER(pinctrl_mstar) = {
 	.id			= UCLASS_PINCTRL,
 	.of_match		= mstar_pinctrl_ids,
 	.ops			= &mstar_pinctrl_ops,
-	.bind			= dm_scan_fdt_dev,
 	.probe			= mstar_pinctrl_probe,
 	.priv_auto_alloc_size	= sizeof(struct mstar_pinctrl_priv),
 };
