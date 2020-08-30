2828e6501f00de8f7c33a91b444c53b3da5d133c
Daniel Palmer <daniel@0x0f.com>
2020-08-30 19:16:43 +0900

    pinctrl

--- drivers/pinctrl/mstar/pinctrl-mstar.c
+++ drivers/pinctrl/mstar/pinctrl-mstar.c
@@ -1,54 +1,74 @@
 #include <common.h>
 #include <dm.h>
 #include <dm/pinctrl.h>
+#include <linux/bitops.h>
+#include <regmap.h>
+
+#include "pinctrl-mstar.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
 struct mstar_pinctrl_priv {
+	struct regmap *regmap;
 };
 
-struct mstar_pinctrl_group {
-	const char *name;
+struct mstar_pinctrl_selector {
+	const char *groupname;
+	const u16 val;
 };
 
 struct mstar_pinctrl_function {
 	const char *name;
+	const unsigned reg;
+	const u16 mask;
+	const struct mstar_pinctrl_selector *selectors;
+	unsigned numselectors;
 };
 
 struct mstar_pinctrl_data {
-	const struct mstar_pinctrl_group *groups;
-	unsigned numgroups;
 	const struct mstar_pinctrl_function *functions;
 	unsigned numfunctions;
+	const char** groupnames;
+	unsigned numgroupnames;
 };
 
-static const struct mstar_pinctrl_group msc313_groups[] = {
-	{ .name = "eth" },
+static const char* msc313_group_names[] = {
+	GROUPNAME_ETH
+};
+
+static const struct mstar_pinctrl_selector selectors_eth[] = {
+	{ .groupname = GROUPNAME_ETH, .val = BIT(2) },
 };
 
 static const struct mstar_pinctrl_function msc313_functions[] = {
-	{ .name = "eth" },
+	{
+		.name = FUNCTIONNAME_ETH,
+		.reg = REG_JTAG,
+		.mask = MASK_ETH,
+		.selectors = selectors_eth,
+		.numselectors = ARRAY_SIZE(selectors_eth),
+	},
 };
 
 static const struct mstar_pinctrl_data msc313_data = {
-	.groups = msc313_groups,
-	.numgroups = ARRAY_SIZE(msc313_groups),
 	.functions = msc313_functions,
 	.numfunctions = ARRAY_SIZE(msc313_functions),
+	.groupnames = msc313_group_names,
+	.numgroupnames = ARRAY_SIZE(msc313_group_names),
 };
 
 static int mstar_pinctrl_get_groups_count(struct udevice *dev)
 {
 	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
 
-	return data->numgroups;
+	return data->numgroupnames;
 }
 
 static const char* mstar_pinctrl_get_group_name(struct udevice *dev, unsigned selector)
 {
 	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
 
-	return data->groups[selector].name;
+	return data->groupnames[selector];
 }
 
 static int  mstar_pinctrl_get_functions_count(struct udevice *dev)
@@ -67,9 +87,25 @@ static const char* mstar_pinctrl_get_function_name(struct udevice *dev, unsigned
 
 static int mstar_pinctrl_pinmux_group_set(struct udevice *dev, unsigned group_selector, unsigned func_selector)
 {
+	struct mstar_pinctrl_priv *priv = dev_get_priv(dev);
+	struct mstar_pinctrl_data *data = dev_get_driver_data(dev);
+	const struct mstar_pinctrl_function *function = &data->functions[func_selector];
+	const struct mstar_pinctrl_selector *selector;
+	const char *groupname = data->groupnames[group_selector];
+	int i = 0;
+
 	printf("mstar pinctrl g %d, f %d\n", group_selector, func_selector);
 
-	return 0;
+	for(i = 0; i < function->numselectors; i++){
+		selector = &function[i];
+		if(strcmp(groupname, selector->groupname) == 0){
+			printf("found selector\n");
+			regmap_update_bits(priv->regmap, function->reg, function->mask, selector->val);
+			return 0;
+		}
+	}
+
+	return -EINVAL;
 }
 
 static struct pinctrl_ops mstar_pinctrl_ops = {
@@ -83,7 +119,15 @@ static struct pinctrl_ops mstar_pinctrl_ops = {
 
 int mstar_pinctrl_probe(struct udevice *dev)
 {
-	return 0;
+	struct mstar_pinctrl_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = regmap_init_mem(dev_ofnode(dev), &priv->regmap);
+	if(ret)
+		goto out;
+
+out:
+	return ret;
 }
 
 static const struct udevice_id mstar_pinctrl_ids[] = {
