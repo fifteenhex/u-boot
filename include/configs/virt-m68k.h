0a2425f61e33441507d719fbb5547106bc8c831a
Daniel Palmer <daniel@0x0f.com>
2024-01-12 21:21:21 +0900

    default command line

--- include/configs/virt-m68k.h
+++ include/configs/virt-m68k.h
@@ -1,3 +1,6 @@
 #define CFG_SYS_INIT_RAM_ADDR 0
 #define CFG_SYS_INIT_RAM_SIZE 0x200000
-
+#define CFG_EXTRA_ENV_SETTINGS \
+        "bootcmd=virtio scan; fatload virtio 1:1 0x3000000 vmlinux\; bootelf 0x3000000\0" \
+	"autostart=yes\0" \
+	"bootargs=console=ttyGF0 root=/dev/vda rootfstype=squashfs\0"
