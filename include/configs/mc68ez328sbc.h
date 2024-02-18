33fa170baba1d2660d183816309db2f5db93eae6
Daniel Palmer <daniel@0x0f.com>
2024-02-18 17:58:03 +0900

    use lz4 to decompress kernel because our spi is sooooo slooooow

--- include/configs/mc68ez328sbc.h
+++ include/configs/mc68ez328sbc.h
@@ -8,3 +8,9 @@
 #endif
 
 #define CFG_SYS_FLASH_BASE 0x10000000
+
+#define CFG_EXTRA_ENV_SETTINGS \
+		"bootcmd=mmc rescan; fatload mmc 0:1 0x400 vmlinux.mc68ez328.lz4;" \
+		"unlz4 0x400 0x400000 0x300000; bootelf 0x400000\0" \
+		"autostart=yes\0" \
+		"bootargs=earlycon=mc68ez328,0xfffff900 console=ttyDB0 root=/dev/mmcblk0p2 rootfstype=squashfs rootwait\0"
