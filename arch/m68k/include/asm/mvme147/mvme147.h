4df6ae970d565b2f3e67a3cd365e6d3c8f398cf1
Daniel Palmer <daniel@0x0f.com>
2024-07-03 23:23:49 +0900

    mvme

---
+++ arch/m68k/include/asm/mvme147/mvme147.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+#ifndef __MVME147_H__
+#define __MVME147_H__
+
+#include <linux/bitops.h>
+
+#ifdef CONFIG_TARGET_MVME147
+
+#define MVME147_SERIAL0_REG  0xFFFE3002
+#define MVME147_SERIAL0_RXTX 0xFFFE3003
+
+#endif
+#endif
