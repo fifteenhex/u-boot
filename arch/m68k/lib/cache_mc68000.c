03eac47c21df520a96dd3b356137d5dd3462ce01
Daniel Palmer <daniel@0x0f.com>
2024-07-27 13:06:40 +0900

    cache

--- arch/m68k/lib/cache_mc68000.c
+++ arch/m68k/lib/cache_mc68000.c
@@ -2,21 +2,12 @@
 
 #include <cpu_func.h>
 #include <asm/cache.h>
+#include <linux/bitops.h>
 
-void flush_cache(ulong start_addr, ulong size)
-{
-}
+#include <stdio.h>
 
-int icache_status(void)
-{
-	return 0;
-}
-
-int dcache_status(void)
-{
-	return 0;
-}
-
+#if defined(CONFIG_MC68030) || defined(CONFIG_MC68040) || defined(CONFIG_TARGET_QEMU)
+#else
 void icache_enable(void)
 {
 }
@@ -25,8 +16,9 @@ void icache_disable(void)
 {
 }
 
-void icache_invalid(void)
+int icache_status(void)
 {
+	return 0;
 }
 
 void dcache_enable(void)
@@ -37,9 +29,19 @@ void dcache_disable(void)
 {
 }
 
+int dcache_status(void)
+{
+	return 0;
+}
+
+void icache_invalid(void)
+{
+}
+
 void dcache_invalid(void)
 {
 }
+#endif
 
 __weak void invalidate_dcache_range(unsigned long start, unsigned long stop)
 {
@@ -48,4 +50,8 @@ __weak void invalidate_dcache_range(unsigned long start, unsigned long stop)
 __weak void flush_dcache_range(unsigned long start, unsigned long stop)
 {
 	/* An empty stub, real implementation should be in platform code */
+}
+
+void flush_cache(ulong start_addr, ulong size)
+{
 }
