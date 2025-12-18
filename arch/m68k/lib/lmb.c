bb4fd933651515750577729aa50faf8edeefa352
Daniel Palmer <daniel@thingy.jp>
2025-12-19 07:06:02 +0900

    m68k: lmb: Reserve the vector table on 68000
    68000 doesn't have a vbar so we cannot move the vector table and
    it is always at 0x0 which is a prime target to get overwritten.
    
    Reserve it in the lmb so at least stuff that checks the lmb cannot
    smush it.

---
+++ arch/m68k/lib/lmb.c
@@ -0,0 +1,13 @@
+#include <lmb.h>
+
+void m68k_lmb_protect_vectors(void)
+{
+#ifndef CONFIG_M68K_HAVE_VBAR
+	int ret;
+
+	/* Don't let the vectors get overwritten if there is no vbar to move the table away from 0x0*/
+	ret = lmb_alloc_mem(LMB_MEM_ALLOC_ADDR, 0, 0x0, 0x400, LMB_NONE);
+		if (ret)
+			printf("mmm %d\n", ret);
+#endif
+}
