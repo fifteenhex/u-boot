d0b2ea925761b1723a12589a85f0062154c6650b
Daniel Palmer <daniel@thingy.jp>
2025-12-21 17:34:18 +0900

    lmb vector thing

--- arch/m68k/lib/lmb.c
+++ arch/m68k/lib/lmb.c
@@ -6,7 +6,7 @@ void m68k_lmb_protect_vectors(void)
 	int ret;
 
 	/* Don't let the vectors get overwritten if there is no vbar to move the table away from 0x0*/
-	ret = lmb_alloc_mem(LMB_MEM_ALLOC_ADDR, 0, 0x0, 0x400, LMB_NONE);
+	ret = lmb_alloc_mem(LMB_MEM_ALLOC_ADDR, 0, 0x0 + 0x4, 0x400, LMB_NOOVERWRITE);
 		if (ret)
 			printf("mmm %d\n", ret);
 #endif
