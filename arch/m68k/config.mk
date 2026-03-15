cf64332f3a5729a47e990e1f216c5e24c6191294
Daniel Palmer <daniel@thingy.jp>
2026-03-15 22:05:55 +0900

    relocation

--- arch/m68k/config.mk
+++ arch/m68k/config.mk
@@ -3,10 +3,7 @@
 # (C) Copyright 2000-2002
 # Wolfgang Denk, DENX Software Engineering, wd@denx.de.
 
-PLATFORM_CPPFLAGS += -D__M68K__
-ifneq ($(CONFIG_M680x0),y)
-PLATFORM_CPPFLAGS += -fPIC
-endif
+PLATFORM_CPPFLAGS += -D__M68K__ -fPIC
 KBUILD_LDFLAGS    += -n -pie
 PLATFORM_RELFLAGS += -ffunction-sections -fdata-sections
 PLATFORM_RELFLAGS += -ffixed-d7
