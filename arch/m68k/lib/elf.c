ec8facc3411097af721e6e072613173ef1cb540a
Daniel Palmer <daniel@thingy.jp>
2026-04-18 21:36:24 +0900

    bootinfo elf bits

--- arch/m68k/lib/elf.c
+++ arch/m68k/lib/elf.c
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0+
 
+#include <asm/global_data.h>
 #include <elf.h>
 #include <stdio.h>
+
+DECLARE_GLOBAL_DATA_PTR;
 
 unsigned long bootelf_exec(ulong (*entry)(int, char * const[]),
 			   ulong end, int argc, char *const argv[])
 {
 
-	if (IS_ENABLED(HAVE_BOOTINFO)) {
 		printf("setting up bootinfo\n");
 
+	memcpy((void *) end, gd->arch.saved_bootinfo, 4096);
+
+	if (IS_ENABLED(M68K_HAVE_BOOTINFO)) {
+		
 	}
 
 	return entry(argc, argv);
